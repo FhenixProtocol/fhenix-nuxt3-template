ace95bf19eff89dbebc144d7f31744cfa034cfa2
Tovi Levis <tovi@scrtlabs.com>
2024-02-13 23:04:54 +0200

    Change http to https in the dummy explorer url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     public: {
       NETWORK_CHAIN_ID: process.env.NUXT_ENV_NETWORK_CHAIN_ID || "412346",
       NETWORK_RPC_URL: process.env.NUXT_ENV_NETWORK_RPC_URL || "https://test01.fhenix.zone/evm",
-      NETWORK_EXPLORER_URL: process.env.NUXT_ENV_NETWORK_EXPLORER_URL || "http://your.explorer.url.here"
+      NETWORK_EXPLORER_URL: process.env.NUXT_ENV_NETWORK_EXPLORER_URL || "https://your.explorer.url.here"
     }
   },  
 })
